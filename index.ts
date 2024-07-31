e35c0e3b3b1dfea4782219f45ebee756767dcd04
Jorge Brown <j.brown@moovit-sp.com>
2024-07-31 15:31:30 +0100

    feat: add catalog and node definitions

---
+++ index.ts
@@ -0,0 +1,3 @@
+import Catalog from "./lib/Catalog";
+
+export default new Catalog("example name", "example description", "https://app.helmut.cloud/img/logo_white.webp")
---
+++ lib/Node.ts
@@ -0,0 +1,61 @@
+import type HCloud from "hcloud-sdk";
+import type { StreamResult } from "hcloud-sdk/lib/interfaces/high5/space/event/stream";
+import type { StreamNodeOutput } from "hcloud-sdk/lib/interfaces/high5/space/event/stream/node";
+import type { StreamNodeSpecification, StreamNodeSpecificationInput } from "hcloud-sdk/lib/interfaces/high5/wave";
+
+export default abstract class Node {
+    public static _isWaveNode = true;
+    wave!: WaveHelper; // This property will be inserted at runtime don't implement it
+    abstract specification: StreamNodeSpecification
+    abstract execute(): Promise<void>
+}
+
+export type WaveHelper = {
+    general: General;
+    logger: Logger;
+    inputs: Inputs;
+    outputs: Outputs;
+}
+
+type General = {
+    getNodeUuid(): string;
+    resolveValue(value: string): number | string;
+    getNodeSpecification(): StreamNodeSpecification;
+    cancelExecution(): void;
+    isCanceled(): boolean;
+    getHcloudClient(): HCloud;
+    getOrgName(): string
+    getSpaceName(): string
+    setStreamMessage(message: string): void
+}
+
+type Logger = {
+    getCurrentProgress(): number | undefined
+    getCurrentMessage(): string | undefined
+    updateProgress(progress: number): void
+    updateMessage(message: string): void
+    updateProgressAndMessage(progress: number, message: string): void
+}
+
+type Inputs = {
+    getInputs(): StreamNodeResolvedInput[]
+    getInputByName(inputName: string): StreamNodeResolvedInput | undefined
+    getPreresolvedInputByName(inputName: string): StreamNodeSpecificationInput | undefined
+    getInputValueByInputName(inputName: string): any
+    getInputOriginalValueByInputName(inputName: string): any
+}
+
+type StreamNodeResolvedInput = {
+    name: string;
+    value: any;
+    originalValue: any;
+}
+
+
+type Outputs = {
+    getAllOutputs(): StreamNodeOutput[]
+    getOutputByName(outputName: string): StreamNodeOutput | undefined
+    getOutputValueByOutputName(outputName: string): any
+    setOutput(name: string, value: any): void
+    executeAdditionalConnector(connectorName: string): Promise<StreamResult | undefined>
+}
