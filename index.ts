a27a6c6cba184280ae9e138450d05a3ab53212e7
Jorge Brown <j.brown@moovit-sp.com>
2024-07-31 15:33:06 +0100

    docs: add sample node

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 import Catalog from "./lib/Catalog";
+import HttpClientAction from "./lib/nodes/HttpClientAction";
 
-export default new Catalog("example name", "example description", "https://app.helmut.cloud/img/logo_white.webp")
+export default new Catalog("example name", "example description", "https://app.helmut.cloud/img/logo_white.webp", HttpClientAction)
