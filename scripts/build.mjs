1b88c68389c6ef8fa060ec858bfa6bad43fc382c
Jorge Brown <j.brown@moovit-sp.com>
2024-10-15 10:49:43 +0200

    feat: allow different node specs per catalog
    Allows for more incremental updates on catalogs

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,35 +3,25 @@ import chalk from "chalk";
 import path from "path";
 
 async function main() {
-	const pkg = JSON.parse(await fs.readFile("./package.json"))
-	const specVersion = pkg.catalogProperties.specVersion;
-	if (specVersion <= 0 || isNaN(specVersion)) {
-		console.error("Invalid specVersion", process.argv[2])
-		process.exit(1)
-	}
-	console.log(`Building specification with spec version ${specVersion}`)
+	console.log(`Building specification`)
 
 	const mod = await import(process.argv[2])
 
 	const catalog = mod.default.default; // Need two defaults because this is a mjs file and the bundle has a non-ESM default export
 
-	const spec = { nodes: [], specVersion };
+	const spec = { nodes: [] };
 
 	for (const [name, nodeCtor] of Object.entries(catalog.nodeCatalog)) {
 		try {
 			// @ts-ignore
 			const node = new nodeCtor();
 			const nodeSpec = node.specification;
-			if (nodeSpec.specVersion === specVersion) {
-				console.log(chalk.green(name))
-				spec.nodes.push(
-					Object.assign(nodeSpec, {
-						path: `name://${name}`
-					})
-				);
-			} else {
-				console.warn(`${chalk.red(name)} specification version does not match. Need ${specVersion} has ${nodeSpec.specVersion}. Skipping`)
-			}
+			console.log(chalk.green(name))
+			spec.nodes.push(
+				Object.assign(nodeSpec, {
+					path: `name://${name}`
+				})
+			);
 		} catch (err) {
 			console.error(err, name);
 		}
