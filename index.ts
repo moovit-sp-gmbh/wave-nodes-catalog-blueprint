5c6f404fa5eafc208fe59fe5b8e4f6f3e9e0e8b9
DariusWe <dariusweiberg@gmail.com>
2024-11-20 14:41:15 +0100

    chore: update NodeSpecification and rename HttpClientAction to HttpClient

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import Catalog from "./lib/Catalog";
-import HttpClientAction from "./lib/nodes/HttpClientAction";
+import HttpClient from "./lib/nodes/HttpClient";
 
 export default new Catalog(
     "example name",
     "example description",
     "https://app.helmut.cloud/img/logo_white.webp",
     "1.0.0",
-    HttpClientAction
+    HttpClient
 );
