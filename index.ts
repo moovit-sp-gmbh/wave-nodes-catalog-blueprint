2dd77434029c7e3972070bd57a18f1b00c59e718
DariusWe <dariusweiberg@gmail.com>
2024-11-26 20:42:56 +0100

    chore: increase minEngineVersion in index.ts

--- index.ts
+++ index.ts
@@ -5,6 +5,6 @@ export default new Catalog(
     "example name",
     "example description",
     "https://app.helmut.cloud/img/logo_white.webp",
-    "1.0.0",
+    "1.5.0-dev-6",
     HttpClient
 );
